2247af78b29910eb2c7cb54a4d13f7d7f3573e31
AlfaZidan <alfazidan76@gmail.com>
2022-04-19 16:35:52 +0700

    responsi

---
+++ Karyawan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4315
+{
+    class Karyawan
+    {
+        private string nama;
+        private string nik;
+        private int gajibulanan;
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public string Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+
+        public int GajiBulanan
+        {
+            get { return gajibulanan; }
+            set { gajibulanan = value; }
+        }
+        public Karyawan(string nama, string nik, int gajibulanan)
+        {
+            this.nama = nama;
+            this.nik = nik;
+            this.gajibulanan = gajibulanan < 0 ? 0 : gajibulanan
+        }
+
+        public void CetakHasil()
+        {
+            Console.WriteLine("{0} {1}      {2}", nik, nama, gajibulanan);
+        }
+
+        public void gajinaik()
+        {
+            int naik;
+            naik = gajibulanan * 110;
+            gajibulanan = naik / 100;
+            Console.WriteLine("naik gaji 10%");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4315
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("190302123", "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan("190302123", "Paijo", 2000000);
+
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("=============================");
+            Console.Write("1 ");karyawan1.CetakHasil();
+            Console.Write("2 ");karyawan1.CetakHasil();
+            Console.WriteLine();
+            karyawan1.naik();
+            karyawan2.naik();
+
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.CetakHasil();
+            Console.Write("2 "); karyawan2.CetakHasil();
+
+            Console.WriteLine("tekan sembarang untuk keluar");
+            Console.ReadKey();
+        }
+    }
+}
